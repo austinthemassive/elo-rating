b09a20f3e031741113bc8fd1fb3acc7a20315ba5
Austin Hayden <austinthemassive@gmail.com>
2016-11-09 09:05:33 -0700

    First commit of elo rating to setup project

---
+++ elorating.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+'''
+This is elo for foosball. Steps:
+	1. handle win of higher rated player, loss of lower rated player
+	2. handle loss of higher rated player, win of lower rated player
+	3. add new player
+	4. display rankings
+
+
+'''
