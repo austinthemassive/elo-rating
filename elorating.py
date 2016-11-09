05be6686e11139383934ab91ac88b018838c4d1a
Austin Hayden <austinthemassive@gmail.com>
2016-11-09 09:18:20 -0700

    adding some additional ideas

--- elorating.py
+++ elorating.py
@@ -2,7 +2,9 @@
 
 '''
 This is elo for foosball. Steps:
-	1. handle win of higher rated player, loss of lower rated player
+	1. handle win of higher rated player, loss of lower rated player. Points allocated from loser to winner
+		What is the function that determines the size of win/loss?
+
 	2. handle loss of higher rated player, win of lower rated player
 	3. add new player
 	4. display rankings
