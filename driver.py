9bf86fe9965bc53e4c88fbf93e8335de7a211c42
Austin Hayden <austinthemassive@gmail.com>
2016-11-10 10:03:50 -0700

    files now added

---
+++ driver.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+'''
+This is elo for foosball. Steps:
+	Read file
+	decode 64 bit?
+	Create Objects
+	Display rankings
+
+	Enter winner
+	Enter loser
+	Enter score differential?
+	Calculate elo delta
+	Apply elo delta
+	Adjust K value (if appropriate)
+
+	Add player
+
+	Reset season
+
+	encode 64 bit?
+	Write file
+	Display Rankings
+
+
+	
+	
+
+	include magnitude of the win? need to research
+	include decay? Probably
+'''
