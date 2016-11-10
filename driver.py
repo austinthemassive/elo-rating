0ecf0e6e0f260cb8ac659cf6e0f0a16f3eb570de
Austin Hayden <austinthemassive@gmail.com>
2016-11-10 13:08:30 -0700

    added player class and methods

--- driver.py
+++ driver.py
@@ -10,9 +10,9 @@ This is elo for foosball. Steps:
 	Enter winner
 	Enter loser
 	Enter score differential?
-	Calculate elo delta
-	Apply elo delta
-	Adjust K value (if appropriate)
+	Calculate elo delta (using gamesplayed and appropriate K value)
+	(X) Apply elo delta
+	
 
 	Add player
 
@@ -29,3 +29,20 @@ This is elo for foosball. Steps:
 	include magnitude of the win? need to research
 	include decay? Probably
 '''
+
+#player class
+
+class Player:
+	'''The default values for a new player should be elo = 1000 and gamesplayed = 0 '''
+	def __init__(self, playername, elo, gamesplayed):
+		self.playername = playername
+		self.elo = elo
+		self.gamesplayed = gamesplayed
+
+	def subtractElo(delta):
+		elo -= delta
+		gamesplayed += 1
+
+	def addElo(delta):
+		elo += delta
+		gamesplayed += 1
