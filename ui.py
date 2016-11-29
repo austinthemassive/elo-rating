8ad8de36a26d00d39de516c7331ae1e984027f1f
Austin Hayden <austinthemassive@gmail.com>
2016-11-29 10:46:54 -0700

    overlap checking finished, cleaned up file

--- ui.py
+++ ui.py
@@ -24,10 +24,7 @@ def randomCircles(screen, numberofparticles, width, height):
 			size = random.randint(5,25)
 			x = random.randint(size,width-size)
 			y = random.randint(size,height-size)
-
-			#compare
 			boolean = _overlap(x,y,size,ListofParticles)
-			print(boolean) #boolean is returning None
 
 		newparticle = Particle(x,y,size)
 		newparticle.display(screen)
@@ -36,49 +33,25 @@ def randomCircles(screen, numberofparticles, width, height):
 	return ListofParticles
 
 def _overlap(x,y,size,ListofParticles):
-
 	for particle in ListofParticles:
 		greaterX = _compareX(particle.x, x)
 		greaterY = _compareY(particle.y, y)
 		x_distance = _calculateDistance(greaterX,particle.x,x)
 		y_distance = _calculateDistance(greaterY,particle.y,y)
-
 		if _hypotenuse(particle.size,size,x_distance,y_distance) == True:
 			continue
-		# if greaterX == 1 and greaterY == 1 and _hypotenuse(particle.size,size,x_distance,y_distance) == True:
-		# 	continue
-		# elif greaterX == 1 and greaterY == 0 and _hypotenuse(particle.size,size,x_distance,y_distance) == True:
-		# 	continue
-		# elif greaterX == 1 and greaterY == -1:
-		# 	continue
-		# elif greaterX == 0 and greaterY == 1:
-		# 	continue
-		# elif greaterX == 0 and greaterY == 0:
-		# 	boolean = True
-		# 	break
-		# elif greaterX == 0 and greaterY == -1:
-		# 	continue
-		# elif greaterX == -1 and greaterY == 1:
-		# 	continue
-		# elif greaterX == -1 and greaterY == 0:
-		# 	continue
-		# elif greaterX == -1 and greaterY == -1:
-		# 	continue
 		else:
 			return True
 
-		#if no shapes overlap, return overlap will return false (to exit previous loop)
-		return False
+	#if no shapes overlap, return overlap will return false (to exit previous loop)
+	return False
 
 def _hypotenuse(radius1,radius2,x_distance,y_distance):
 	hypotenuse = math.sqrt(x_distance**2+y_distance**2)
-	print("\nhypotenuse: ",hypotenuse)
-	print("added radius",radius1+radius2)
 	if hypotenuse >= (radius1+radius2):
 		#return true if not overlapping
 		return True
 	else:
-		# print("****************************************")
 		#return false if overlapping
 		return False	
 
@@ -116,9 +89,6 @@ screen = display.set_mode((width,height))
 screen.fill(backgroundcolor)
 display.set_caption('UI experiments')
 
-# #draw a circle at these coordinates and this size
-# myparticle = Particle(150,50,50)
-# myparticle.display(screen)
 
 #randomly generate 10 particles
 randomCircles(screen, 10, width, height)
