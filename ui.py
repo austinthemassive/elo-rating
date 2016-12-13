9cd7c4c55fe23c8ebb47f54852889e80630c8189
Austin Hayden <austinthemassive@gmail.com>
2016-12-13 12:55:27 -0700

    added in the ability to click on a circle and select it.

--- ui.py
+++ ui.py
@@ -5,14 +5,14 @@ import random
 import math
 
 # set up the screen
-windowWidth = 1000
+windowWidth = 600
 windowHeight = 600
 backgroundcolor = (150,150,150)
 screen = display.set_mode((windowWidth,windowHeight))
 screen.fill(backgroundcolor)
 display.set_caption('UI experiments')
 
-gravity = (math.pi,.002)
+gravity = (math.pi,.001)
 
 # Particle Class
 class Particle:
@@ -20,14 +20,18 @@ class Particle:
 	thickness = 1
 	angle = 0
 	speed = 0
+	drag = 0
+	elasticity = 0
 	
 	def __init__(self,x,y,size):
 		self.x = x
 		self.y = y
 		self.size = size
 		# self.speed = random.random()
-		self.speed = random.uniform(0,.8)
+		self.speed = random.uniform(0,1)
 		self.angle = random.uniform(0,2*math.pi)
+		self.elasticity = .75
+		self.drag = .9999999999
 
 	
 	def display(self, screen1):
@@ -35,12 +39,26 @@ class Particle:
 		draw.circle(screen1, self.color, (int(self.x),int(self.y)),self.size, self.thickness)
 
 	def move(self):
-		#add the vectors
+		#factor in gravity's vector
 		(self.angle, self.speed) = self.addVectors(self.angle,self.speed,gravity[0], gravity[1])
 		
 		#move shape accordingly
 		self.x += math.sin(self.angle)*self.speed
 		self.y -= math.cos(self.angle)*self.speed
+		
+		#simulate drag according to shape size
+		self.speed *= (1-self.size/1000000000)
+
+	def fling(self,mouseAngle,mouseSpeed):
+		#factor in mouse fling vector
+		(self.angle, self.speed) = self.addVectors(self.angle,self.speed,mouseAngle,mouseSpeed)
+		
+		#move shape accordingly
+		self.x += math.sin(self.angle)*self.speed
+		self.y -= math.cos(self.angle)*self.speed
+		
+		#simulate drag according to shape size
+		self.speed *= (1-self.size/1000000000)
 
 	def addVectors(self,angle1,length1,angle2,length2):
 			tempx = math.sin(angle1)*length1+math.sin(angle2)*length2
@@ -127,36 +145,47 @@ def hardWall(windowWidth, windowHeight, particle):
 	if particle.x > windowWidth-particle.size:
 		particle.x = 2*(windowWidth - particle.size) - particle.x
 		particle.angle = -particle.angle
+		particle.speed *= particle.elasticity
 	elif particle.x < particle.size:
 		particle.x = 2*(particle.size) - particle.x
 		particle.angle = -particle.angle
-	
+		particle.speed *= particle.elasticity
 	#handle y
 	if particle.y > windowHeight-particle.size:
 		particle.y = 2*(windowHeight - particle.size) - particle.y
 		particle.angle = math.pi-particle.angle
+		particle.speed *= particle.elasticity
 	elif particle.y < particle.size:
 		particle.y = 2*(particle.size) - particle.y
 		particle.angle = math.pi-particle.angle
+		particle.speed *= particle.elasticity
 
-	
+def selectParticle(particleList,xMouse,yMouse):
+	for particle in particleList:
+		if math.hypot(particle.x-xMouse, particle.y-yMouse) <= particle.size:
+			return particle
+		else:
+			return None
 
 
 
-#randomly generate 10 particles
-particleList = randomCircles(screen, 1, windowWidth, windowHeight)
-
+#randomly generate a certain number of particles
+particleList = randomCircles(screen, 3, windowWidth, windowHeight)
+selected_Particle=None
 running = True
 while running:
 	screen.fill(backgroundcolor)
 	for particle in particleList:
-		# particle.speed = random.random()
-		# particle.angle = random.uniform(0,2*math.pi)
-		particle.move()
-		hardWall(windowWidth,windowHeight,particle)
+		if particle != selected_Particle:
+			particle.move()
+			hardWall(windowWidth,windowHeight,particle)
 		particle.display(screen)
 	display.flip()
 	for occurence in event.get():
 		if occurence.type == QUIT:
 			running = False
-
+		if occurence.type == MOUSEBUTTONDOWN:
+			(xMouse,yMouse) = mouse.get_pos()
+			selected_Particle = selectParticle(particleList,xMouse,yMouse)
+		if occurence.type == MOUSEBUTTONUP:
+			selected_Particle = None
